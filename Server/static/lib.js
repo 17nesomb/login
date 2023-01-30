1dc38c8726f7e0ffc2a51be3ab9a3338e4b81a92
Benjamin Nesom <17NesomB@fulford.york.sch.uk>
2023-01-30 11:17:54 +0000

    Fixed everything ever

---
+++ Server/static/lib.js
@@ -0,0 +1,37 @@
+const eHints = document.getElementById("hints");
+
+function reset() {
+    
+}
+
+function create() {
+
+}
+
+function login() {
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+
+    try {
+        presenceCheck(username, "Enter something (your username) fool");
+        presenceCheck(password, "PASSWORD FOOL");
+        lengthCheck(password, 5, 10, "please enter a password between 5 and 10 characters");
+        eHints.innerText = "";
+    } catch (e) {
+        eHints.innerText = e;
+    }
+
+}
+
+function lengthCheck(input, minLength, maxLength, message) {
+    if (input.length > maxLength || input.length < minLength) {
+        throw message;
+    }
+}
+
+function presenceCheck(input, message) {
+
+    if (input == "") {
+        throw message;
+    }
+}
